a30f79bcdb723f2a3661fdd2c4357c5d2e6be20d
Kexin Song <22322308+kathy9980@users.noreply.github.com>
2024-07-02 12:18:38 -0400

    Add files via upload

--- Others/convertDate.m
+++ Others/convertDate.m
@@ -1,4 +1,26 @@
 function datestring_new = convertDate(datestring,format)
+% CONVERTDATE Convert a date string to a specified format.
+%
+% This function converts a date string to one of three specified formats.
+%
+% Input:
+%   datestring: The input date string, either YYYYmmdd or YYYY-mm-dd.
+%   format: The target date format. Can be one of:
+%           - 'YYYYdoy': Converts the date to "year and day of year" format.
+%           - 'YYYYmmdd': Converts the date to "year, month, and day" format without dashes.
+%           - 'YYYY-mm-dd': Converts the date to "year, month, and day" format with dashes.
+%
+% Output:
+%   datestring_new: The date string converted to the specified format.
+%
+% Example usage:
+%   datestring_new = convertDate('2021-07-01', 'YYYYdoy');
+%   datestring_new = convertDate('2021-07-01', 'YYYYmmdd');
+%   datestring_new = convertDate('20210701', 'YYYY-mm-dd');
+%
+% Author: Kexin Song (kexin.song@uconn.edu
+% Date: 2024/07/02
+
     switch format
         case 'YYYYdoy'
             YYYYdoy = date2doy(datestring);
